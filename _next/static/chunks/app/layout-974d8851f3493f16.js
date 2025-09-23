db2ac6e36c79672a41ea653b9155dce3462d6b2d
Mohamad Termanini <mohtermanini.job@gmail.com>
2025-09-23 21:28:44 +0400

    Updates

---
+++ _next/static/chunks/app/layout-974d8851f3493f16.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1671:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,9324,23)),Promise.resolve().then(l.t.bind(l,9840,23))},9324:()=>{},9840:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_f07e2c"}}},e=>{var s=s=>e(e.s=s);e.O(0,[385,441,684,358],()=>s(1671)),_N_E=e.O()}]);
